27dcac82e212bb4600baa48f3ace6f2479a7293f
Grant Madison <g.madison2897@yahoo.com>
2026-03-06 09:38:11 -0600

    rebranding

--- server/notification-providers/google-chat.js
+++ server/notification-providers/google-chat.js
@@ -52,7 +52,7 @@ class GoogleChat extends NotificationProvider {
             }
 
             let chatHeader = {
-                title: "Uptime Kuma Alert",
+                title: "Madi Monitor Alert",
             };
 
             if (monitorJSON && heartbeatJSON) {
@@ -98,7 +98,7 @@ class GoogleChat extends NotificationProvider {
                     buttonList: {
                         buttons: [
                             {
-                                text: "Visit Uptime Kuma",
+                                text: "Visit Madi Monitor",
                                 onClick: {
                                     openLink: {
                                         url: baseURL + urlPath,
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
  * node "server/server.js"
  * DO NOT require("./server") in other modules, it likely creates circular dependency!
  */
-console.log("Welcome to Uptime Kuma");
+console.log("Welcome to Madi Monitor");
 
 // As the log function need to use dayjs, it should be very top
 const dayjs = require("dayjs");
@@ -68,7 +68,7 @@ if (process.env.UPTIME_KUMA_WS_ORIGIN_CHECK === "bypass") {
 }
 
 const checkVersion = require("./check-version");
-log.info("server", "Uptime Kuma Version:", checkVersion.version);
+log.info("server", "Madi Monitor Version:", checkVersion.version);
 
 log.info("server", "Loading modules");
 
@@ -690,7 +690,7 @@ let needSetup = false;
 
                 if ((await R.knex("user").count("id as count").first()).count !== 0) {
                     throw new Error(
-                        "Uptime Kuma has been initialized. If you want to run setup again, please delete the database."
+                        "Madi Monitor has been initialized. If you want to run setup again, please delete the database."
                     );
                 }
 
